69802137f45535899b482942c0ef5fce6c89979c
Caio Monte <caiomm076@gmail.com>
2024-12-23 21:30:31 -0300

    estudo js

--- JavaScript/4_funcoes.js
+++ JavaScript/4_funcoes.js
@@ -1,15 +1,8 @@
-function criarContador() {
-    let contador = 0;
-
-    return function incrementar() {
-        contador++;
-        return contador;
-    };
-}
-
-const contador1 = criarContador(); // Cria um closure
-console.log(contador1()); // Saída: 1
-console.log(contador1()); // Saída: 2
-
-const contador2 = criarContador(); // Cria outro closure independente
-console.log(contador2()); // Saída: 1
+function multiplicar(a, b = 1) {
+  
+    return a * b;
+  }
+  
+  console.log(multiplicar(5, 2)); // 10
+  console.log(multiplicar(5)); // 5
+  
