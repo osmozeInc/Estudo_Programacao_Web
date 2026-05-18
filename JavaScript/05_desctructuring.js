09a27a1b7f5b24b357f939d1a408bc1bd1a40ee2
PC - Casa Linux <caiomm076@gmail.com>
2026-05-18 18:30:40 -0300

    estudo de sistaxe moderna em JavaScript

---
+++ JavaScript/05_desctructuring.js
@@ -0,0 +1,20 @@
+// desestruturação de array
+const numeros = [1, 2, 3]
+const [p, s] = numeros
+
+// desestruturação de objeto
+const pessoa = {
+    nome: "caio",
+    idade: 15
+};
+const {nome, idade} = pessoa
+
+// saidas
+console.log(p)
+console.log(s)
+console.log(nome)
+console.log(idade)
+
+// tipos
+console.log(typeof p)
+console.log(typeof nome)
